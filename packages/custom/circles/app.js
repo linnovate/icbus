074f2f7d2dcadfe349fd4cb7bb39233830979fd3
Yonatan Ellman <yonatan@nodeside.com>
2015-05-27 16:51:43 +0300

    added icapi b ack and basic setup

--- packages/custom/circles/app.js
+++ packages/custom/circles/app.js
@@ -15,38 +15,8 @@ Circles.register(function(app, auth, database) {
 
   //We enable routing. By default the Package Object is passed to the routes
   Circles.routes(app, auth, database);
-
-  //We are adding a link to the main menu for all authenticated users
-  Circles.menus.add({
-    title: 'circles example page',
-    link: 'circles example page',
-    roles: ['authenticated'],
-    menu: 'main'
-  });
-  
+ 
   Circles.aggregateAsset('css', 'circles.css');
-
-  /**
-    //Uncomment to use. Requires meanio@0.3.7 or above
-    // Save settings with callback
-    // Use this for saving data from administration pages
-    Circles.settings({
-        'someSetting': 'some value'
-    }, function(err, settings) {
-        //you now have the settings object
-    });
-
-    // Another save settings example this time with no callback
-    // This writes over the last settings.
-    Circles.settings({
-        'anotherSettings': 'some value'
-    });
-
-    // Get settings. Retrieves latest saved settigns
-    Circles.settings(function(err, settings) {
-        //you now have the settings object
-    });
-    */
 
   return Circles;
 });
---
+++ packages/custom/icapi/server/routes/icapi.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var projectController = require('../controllers/project');
+var taskController = require('../controllers/task');
+
+var permissionController = require('../controllers/permission');
+
+module.exports = function(Icapi, app, auth, database) {
+
+  app.route('/api/icapi/projects/:id?')
+    .all(auth.requiresLogin, permissionController.echo)
+    //.post(permissionController.forceLogIn, projectController.create)     	//Create
+    .post(projectController.create)     	//Create
+    .get(projectController.read)        									//Read
+    .put(projectController.update)      									//Update
+    .delete(projectController.destroy); 									//Delete
+
+  app.route('/api/icapi/tasks/:id?')
+    .all(permissionController.echo)
+    //.post(permissionController.forceLogIn, taskController.create)
+    .post(taskController.create)
+    .get(taskController.read)
+    .put(taskController.update)
+    .delete(taskController.destroy);
+};
