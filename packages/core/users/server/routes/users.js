2f158282f20a4098cd57b5662395340880e8fa0b
Yonatan Ellman <yonatan@nodeside.com>
2015-06-03 15:32:01 +0300

    fixing bad merges etc

--- packages/core/users/server/routes/users.js
+++ packages/core/users/server/routes/users.js
@@ -1,13 +1,8 @@
 'use strict';
 
-<<<<<<< HEAD
 // User routes use users controller
-var users = require('../controllers/users'),
-  config = require('meanio').loadConfig();
-
-=======
 var config = require('meanio').loadConfig();
->>>>>>> 49d48107714cca5798b131b9463c5adf2d793d02
+
 var jwt = require('jsonwebtoken'); //https://npmjs.org/package/node-jsonwebtoken
 
 module.exports = function(MeanUser, app, auth, database, passport) {
@@ -49,9 +44,11 @@ module.exports = function(MeanUser, app, auth, database, passport) {
       var escaped = JSON.stringify(payload);
       escaped = encodeURI(escaped);
       // We are sending the payload inside the token
-<<<<<<< HEAD
       var token = jwt.sign(escaped, config.secret, {
         expiresInMinutes: 60 * 5
+      });
+      MeanUser.events.publish('login', {
+        description: req.user.name + ' login to the system.'
       });
 
       var request = require('request');
@@ -60,8 +57,8 @@ module.exports = function(MeanUser, app, auth, database, passport) {
           if (!error && body) {
             if (req.user && req.user._id) {
               //no prtection yet, poc
-              
-              var signature  = JSON.parse(body);
+
+              var signature = JSON.parse(body);
 
               users.updateSignature(req.user, signature, function(err) {
                 console.log(err);
@@ -78,13 +75,7 @@ module.exports = function(MeanUser, app, auth, database, passport) {
       res.json({
         token: token
       });
-=======
-      var token = jwt.sign(escaped, config.secret, { expiresInMinutes: 60*5 });
-      MeanUser.events.publish('login', {
-        description: req.user.name + ' login to the system.'
-      });
-      res.json({ token: token });
->>>>>>> 49d48107714cca5798b131b9463c5adf2d793d02
+
     });
 
   // AngularJS route to get config of social buttons
