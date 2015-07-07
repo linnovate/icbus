fcbab73a1de956321130ead96846bf17c598374a
Rivka Thur <rivkat@linnovate.net>
2015-07-07 15:10:12 +0300

    add profile api

--- packages/custom/icapi/server/routes/icapi.js
+++ packages/custom/icapi/server/routes/icapi.js
@@ -2,27 +2,29 @@
 
 var projectController = require('../controllers/project');
 var taskController = require('../controllers/task');
+var profile = require('../controllers/profile');
 
 var permissionController = require('../controllers/permission');
 
 module.exports = function(Icapi, app, auth, database, elasticsearch) {
 
-  app.route('/api/projects/:id?')
+    app.route('/api/projects/:id?')
     //.all(auth.requiresLogin, permissionController.echo)
     //.post(permissionController.forceLogIn, projectController.create)     	//Create
-    .post(projectController.create)     	//Create
-    .get(projectController.read)        									//Read
-    .put(projectController.update)      									//Update
-    .delete(projectController.destroy);
-    							//Delete
-  app.route('/api/tasks/tags').get(taskController.tagsList);
-  app.route('/api/tasks/:id?')
+    .post(projectController.create) //Create
+    .get(projectController.read) //Read
+    .put(projectController.update) //Update
+    .delete(projectController.destroy); //Delete
+    app.route('/api/tasks/tags').get(taskController.tagsList);
+    app.route('/api/tasks/:id?')
     //.all(permissionController.echo)
     //.post(permissionController.forceLogIn, taskController.create)
     .post(taskController.create)
-    .get(taskController.read)
-    .put(taskController.update)
-    .delete(taskController.destroy);
+        .get(taskController.read)
+        .put(taskController.update)
+        .delete(taskController.destroy);
 
-
+    app.route('/api/profile')
+        .get(profile.profile, profile.show)
+        .put(profile.profile, auth.requiresLogin, profile.update);
 };
