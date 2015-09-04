cbb9ee90cb70a0d17edc208d0017e6bbafbea9fd
Roman Golovin <danteek@gmail.com>
2015-09-04 12:51:47 +0300

    Fix updating enitities

--- packages/custom/icapi/server/controllers/utils.js
+++ packages/custom/icapi/server/controllers/utils.js
@@ -1,6 +1,11 @@
+'use strict';
+
+var inspect = require('util').inspect;
+
 exports.checkAndHandleError = function (err, res, defaultMessage) {
     if (err) {
         var errMesg = defaultMessage || err.errors || err.message || 'Oops...';
+        console.log(inspect(errMesg));
         throw new Error(errMesg);
     }
 };
